dc6c700352304b64248614370c3b092d0e4c0ecd
Michael Voecks <michael.voecks@colorado.edu>
2016-09-25 15:46:41 -0600

    adding files

--- r-testing/testing.r
+++ r-testing/testing.r
@@ -1,16 +1,3 @@
 #!/usr/bin/Rscript
+print(dgeom(0:36500, 1/5040))
 
-distance <- function(x,y){sqrt(x^2+y^2)}
-distance(4,3)
-distance(5,12)
-plotfunction <- function(x){
-  y <- sqrt(x)
-  y <- y+10
-  return(y)
-}
-png("seqtestplot.png")
-plot(seq(1,100,1))
-dev.off()
-jpeg("functionplot.png")
-plot(plotfunction, xlab="X", ylab="Y", main="TITLE", xlim=c(0, 100))
-dev.off()
